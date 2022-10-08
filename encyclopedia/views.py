d7fe89af1df62d2971114b37a5369126b4b3bc77
CamilaNieto-Centennial <cnietomo@my.centennialcollege.ca>
2022-10-07 22:59:32 -0500

    Entry Pages display, and Error Page works

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#import markdown2
-#from markdown2 import Markdown
-import markdown
+import markdown2
 
 from . import util
-
-#markdowner = Markdown()
 
 # Index Page
 def index(request):
@@ -16,14 +12,19 @@ def index(request):
 
 # Entry Page
 def entry(request, name):
-    if util.get_entry(name) == None:
-        return HttpResponse("This page does not exist yet!")
-    else:
-        markdown.markdownFromFile(
-        input = "entries/" + name + ".md",
-        output = "encyclopedia/templates/entries/" + name + ".html",
-        encoding= "utf8"
-        )
-        return render(request, "entries/" + name + ".html", {
-            "name": util.get_entry(name)
+    nameEntry = util.get_entry(name) # Access the .md file
+
+    # Check if that page exists. If not display Error
+    if nameEntry == None:
+        error = "This page does not exist yet!"
+        return render(request, "encyclopedia/error.html", {
+            "error": error
         })
+    # If exists, render "title" and "main" to entry.html file 
+    else:
+        mainContent = markdown2.markdown(nameEntry) # Covert md to HTML
+        return render(request, "encyclopedia/entry.html", {
+            "title": name,
+            "main": mainContent
+        })
+
